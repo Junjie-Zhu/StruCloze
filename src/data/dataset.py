15b16b1282e1026203a2c16db58e2a6f5ed5b0ad
Junjie Zhu <84182340+Junjie-Zhu@users.noreply.github.com>
2024-12-23 10:50:05 +0800

    Update dataset.py

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,10 +3,63 @@ import pickle
 from functools import lru_cache
 from pathlib import Path
 from typing import Union, Optional
+import tree
 
 import numpy as np
 import pandas as pd
 import torch
+
+from src.utils import residue_constants
+
+CA_IDX = residue_constants.atom_order['CA']
+
+
+class ProteinTransform:
+    def __init__(self,
+                 truncate_length: Optional[int] = None,
+                 strip_missing_residues: bool = True,
+                 eps: float = 1e-8,
+    ):
+        if truncate_length is not None:
+            assert truncate_length > 0, 'truncate_length should be greater than 0'
+        self.truncate_length = truncate_length
+        self.strip_missing_residues = strip_missing_residues
+        self.eps = eps
+
+    @staticmethod
+    def patch_feats(chain_feats):
+        seq_mask = chain_feats['atom_mask'][:, CA_IDX]  # a little hack here
+        residue_idx = chain_feats['residue_index'] - np.min(
+            chain_feats['residue_index'])  # start from 0, possibly has chain break
+        patch_feats = {
+            'seq_mask': seq_mask,
+            'residue_mask': seq_mask,
+            'residue_idx': residue_idx,
+            'fixed_mask': np.zeros_like(seq_mask),
+            'sc_ca_t': np.zeros(seq_mask.shape + (3,)),
+        }
+        chain_feats.update(patch_feats)
+        return chain_feats
+
+    @staticmethod
+    def strip_ends(chain_feats):
+        # Strip missing residues on both ends
+        modeled_idx = np.where(chain_feats['aatype'] != 20)[0]
+        min_idx, max_idx = np.min(modeled_idx), np.max(modeled_idx)
+        chain_feats = tree.map_structure(
+            lambda x: x[min_idx: (max_idx + 1)], chain_feats)
+        return chain_feats
+
+    @staticmethod
+    def random_truncate(chain_feats, max_len):
+        L = chain_feats['aatype'].shape[0]
+        if L > max_len:
+            # Randomly truncate
+            start = np.random.randint(0, L - max_len + 1)
+            end = start + max_len
+            chain_feats = tree.map_structure(
+                lambda x: x[start: end], chain_feats)
+        return chain_feats
 
 
 class ProteinDataset(torch.nn.utils.Dataset):
@@ -32,6 +85,21 @@ class ProteinDataset(torch.nn.utils.Dataset):
 
     @lru_cache(maxsize=100)
     def __getitem__(self, idx):
+        """
+        data_object:
+            seq
+            mask
+            pair_mask
+            chain_id
+            dist_one_hot
+            left_gt_calpha3_frame_positions
+            right_gt_calpha3_frame_positions
+
+            # recycle_features
+            prev_seq
+            prev_pair
+        """
+
         data_path = self._data[idx]
         accession_code = os.path.splitext(os.path.basename(data_path))[0]
 
@@ -42,4 +110,5 @@ class ProteinDataset(torch.nn.utils.Dataset):
             data_object = self.transform(data_object)
 
         data_object['accession_code'] = accession_code
+
         return data_object
